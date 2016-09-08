e4514a18b6b7ee028f941ceb6683eafef8a0c8ff
mac-os <bhupati@srivatech.com>
2016-09-08 10:35:32 -0700

    Initial Commit.

---
+++ index.android.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import {
+   AppRegistry,
+   View
+} from 'react-native'
+import Router from './components/HomeContainer'
+
+class Navigation extends Component {
+   render() {
+      return (
+         <HomeContainer />
+      )
+   }
+}
+
+AppRegistry.registerComponent('Switch', () => switch (expression) {
+  case expression:
+
+    break;
+  default:
+
+})
